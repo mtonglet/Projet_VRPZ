028c0affb1537af5a1b7c5db26f78f7b55b31a58
mtonglet <Morgan.Tonglet@ulb.be>
2022-12-29 00:03:30 +0100

    retest de commit

--- Project/headers/shader.h
+++ Project/headers/shader.h
@@ -2,11 +2,12 @@
 #define SHADER_H
 
 #include <glad/glad.h>
-
+#include <vector>
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <vector>
 
 #include <glm/glm.hpp>
 #include<glm/gtc/matrix_transform.hpp>
@@ -17,14 +18,16 @@ class Shader
 {
 public:
 	GLuint ID;
+    
+    Shader();
 
     Shader(const char* vertexPath, const char* fragmentPath);
-	
 
     Shader(std::string vShaderCode, std::string fShaderCode);
 
     Shader(const char* vertexPath, const char* geometryPath, const char* fragmentPath);
 
+//    void drawElements();
     void use();
     void setInteger(const GLchar* name, GLint value);
     void setFloat(const GLchar* name, GLfloat value);
@@ -32,8 +35,11 @@ public:
     void setVector3f(const GLchar* name, const glm::vec3& value);
     void setMatrix4(const GLchar* name, const glm::mat4& matrix);
     void setTexUnit(const GLchar* name, GLuint unit);
+    void setLightsPos(const int nLights, std::vector<glm::vec3> &array);
+    void setLightsParams(const int maxLightsNumber, const float(&params)[6]);
 
 private:
+ //   std::vector<Element> attachedElements;
     GLuint compileShader(std::string shaderCode, GLenum shaderType);
     GLuint compileProgram(GLuint vertexShader, GLuint fragmentShader);
     GLuint compileProgram(GLuint vertexShader, GLuint geometryShader, GLuint fragmentShader);
--- Project/shaders/classic.frag
+++ Project/shaders/classic.frag
@@ -2,8 +2,12 @@
 	out vec4 FragColor;
 	precision mediump float;
 	
-	
+	uniform vec3 colorRGB;
+	uniform float alpha;
+
 	void main()
 	{ 
-		FragColor = vec4(0.5,0.4,0.5,1.0); 
+		FragColor = vec4(colorRGB,alpha);
+		//FragColor = vec4(0.5,0.4,0.5,1.0);
+		//FragColor = vec4(vec3(red,green,blue), 1.0);
 	}
